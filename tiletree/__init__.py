4fe3c077bc73fb450b1d813d9e95c6f9c1c50c38
Benjamin Campbell <benjamin.l.campbell@gmail.com>
2012-06-06 09:54:37 -0500

    File system based slippy map storage.

--- tiletree/__init__.py
+++ tiletree/__init__.py
@@ -3,6 +3,8 @@ import Image
 import ImagePalette
 import ImageDraw
 import StringIO
+import os
+import math
 
 ##\brief A simple, QuadTreeNode
 #
@@ -11,7 +13,7 @@ class QuadTreeGenNode:
 	#
 	def __init__(self, node_id=0, min_x=0, min_y=0, max_x=0, max_y=0, zoom_level=0,
 			image_id=None, is_leaf=True, child_0=None, child_1=None, child_2=None, child_3=None,
-			parent_geom=None):
+			parent_geom=None, tile_x=0, tile_y=0):
 		self.node_id = node_id
 		self.min_x = min_x
 		self.min_y = min_y
@@ -24,12 +26,14 @@ class QuadTreeGenNode:
 		self.child_1 = child_1
 		self.child_2 = child_2
 		self.child_3 = child_3
-		self.parent_geom = None
+		self.parent_geom = parent_geom
+		self.tile_x = tile_x
+		self.tile_y = tile_y
 
 	def to_csv(self):
 		return ','.join(repr(x) for x in [self.node_id, self.zoom_level, self.min_x, self.min_y,
 			self.max_x, self.max_y, self.image_id, self.is_leaf, self.child_0, self.child_1, self.child_2,
-			self.child_3])
+			self.child_3, self.tile_x, self.tile_y])
 
 	def __repr__(self):
 		return repr(self.__dict__)
@@ -40,28 +44,45 @@ class CSVStorageManager:
 		self.image_file = image_file
 		self.image_prefix = image_prefix
 		self.image_suffix = image_suffix
-		#maps image ids to 
-		self.image_dict = {}
 
 		self.tree_file.write(','.join(['node_id', 'zoom_level', 'min_x', 'min_y', 'max_x', 'max_y',
-			'image_id', 'is_leaf', 'child_0', 'child_1', 'child_2', 'child_3']))
+			'image_id', 'is_leaf', 'child_0', 'child_1', 'child_2', 'child_3',
+			'tile_x', 'tile_y']))
 		self.tree_file.write('\n')
 
 		self.image_file.write(','.join(['image_id', 'image_fn']))
 		self.image_file.write('\n')
 
+	def lookup_tile(self, zoom_level, x, y):
+		raise Exception("Not implemented")
+
+	def get_slippy_path(self, node):
+		#store in OSM slippy tile structure
+		link_dir = os.path.join(self.image_prefix, repr(node.zoom_level), repr(node.tile_x))
+		link_fn = os.path.join(link_dir, repr(node.tile_y)) + self.image_suffix
+
+		return (link_dir, link_fn)
+
+	def _get_storage_path(self, node):
+		return self.image_prefix + repr(node.image_id) + self.image_suffix
+
 	def store(self, node, img_bytes):
 		self.tree_file.write(node.to_csv())
 		self.tree_file.write('\n')
 
-		img_fn = self.image_dict.get(node.image_id, None)
-		if(img_fn == None):
-
-			img_fn = self.image_prefix + repr(node.image_id) + self.image_suffix
+		img_fn = self._get_storage_path(node)
+		if(not os.path.exists(img_fn)):
+			#create the image
 			open(img_fn, 'w').write(img_bytes.getvalue())
+			#also output the information to the csv
+			self.image_file.write(','.join([repr(node.node_id), img_fn]))
+			self.image_file.write('\n')
 
-		self.image_file.write(','.join([repr(node.node_id), img_fn]))
-		self.image_file.write('\n')
+		#now create a sym link to the image (slippymap style)
+		link_dir, link_fn = self.get_slippy_path(node)
+		if(not os.path.isdir(link_dir)):
+			os.makedirs(link_dir)
+			os.symlink(img_fn, link_fn)
 
 	def close(self):
 		self.tree_file.close()
@@ -135,40 +156,51 @@ class QuadTreeGenerator:
 			return []
 
 		#calculate new coordinates for child nodes
+		#assume mercator when create tile coordinates
+		#I guess I hate mapping
+		this_zoom = node.zoom_level + 1
 		min_x0 = node.min_x
 		min_y0 = node.min_y
 		max_x0 = node.max_x / 2.0
 		max_y0 = node.max_y / 2.0
+		tile_x0 = node.tile_x * math.exp(2,this_zoom) - 2
+		tile_y0 = node.tile_x * math.exp(2,this_zoom) - 2
 
 		min_x1 = max_x0
 		min_y1 = node.min_y
 		max_x1 = node.max_x
 		max_y1 = max_y0
+		tile_x0 = node.tile_x * math.exp(2,this_zoom) - 1
+		tile_y0 = node.tile_x * math.exp(2,this_zoom) - 2
 
 		min_x2 = node.min_x
 		min_y2 = max_y0
 		max_x2 = max_x0
 		max_y2 = node.max_y
+		tile_x0 = node.tile_x * math.exp(2,this_zoom) - 2
+		tile_y0 = node.tile_x * math.exp(2,this_zoom) - 1
 
 		min_x3 = max_x0
 		min_y3 = max_y0
 		max_x3 = node.max_y
 		max_y3 = node.max_y
+		tile_x0 = node.tile_x * math.exp(2,this_zoom) - 1
+		tile_y0 = node.tile_x * math.exp(2,this_zoom) - 1
 
-		child0 = QuadTreeGenNode(self.next_node_id, min_x0, min_y0, max_x0, max_y0, node.zoom_level+1,
-				parent_geom=geom)
+		child0 = QuadTreeGenNode(self.next_node_id, min_x0, min_y0, max_x0, max_y0, this_zoom,
+				parent_geom=geom, tile_x=tile_x0, tile_y=tile_y0)
 		self.next_node_id += 1
 
-		child1 = QuadTreeGenNode(self.next_node_id, min_x1, min_y1, max_x1, max_y1, node.zoom_level+1,
-				parent_geom=geom)
+		child1 = QuadTreeGenNode(self.next_node_id, min_x1, min_y1, max_x1, max_y1, this_zoom,
+				parent_geom=geom, tile_x=tile_x1, tile_y=tile_y1)
 		self.next_node_id += 1
 
-		child2 = QuadTreeGenNode(self.next_node_id, min_x2, min_y2, max_x2, max_y2, node.zoom_level+1,
-				parent_geom=geom)
+		child2 = QuadTreeGenNode(self.next_node_id, min_x2, min_y2, max_x2, max_y2, this_zoom,
+				parent_geom=geom, tile_x=tile_x2, tile_y=tile_y2)
 		self.next_node_id += 1
 
-		child3 = QuadTreeGenNode(self.next_node_id, min_x3, min_y3, max_x3, max_y3, node.zoom_level+1,
-				parent_geom=geom)
+		child3 = QuadTreeGenNode(self.next_node_id, min_x3, min_y3, max_x3, max_y3, this_zoom,
+				parent_geom=geom, tile_x=tile_x3, tile_y=tile_y3)
 		self.next_node_id += 1
 
 		return (child0, child1, child2, child3)
