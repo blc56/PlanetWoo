7efc6e5f19313ae8c6beb9ac35262ed7a5d44142
Benjamin Campbell <benjamin.l.campbell@gmail.com>
2012-06-15 15:36:04 -0500

    Temporary fix for mapserver BBOX/extents.

--- tiletree/mapserver.py
+++ tiletree/mapserver.py
@@ -20,12 +20,30 @@ class MapServerRenderer(Renderer):
 		self.mapfile = mapscript.fromstring(self.mapfile_template % template_args)
 
 	def render_normal(self, geometry, is_blank, is_full, is_leaf, min_x, min_y, max_x, max_y, zoom_level, tile_x, tile_y):
-		#print zoom_level, tile_x, tile_y, min_x, min_y, max_x, max_y
-		self.mapfile.setExtent(min_x, min_y, max_x, max_y)
+		#aparrently, setExtent() is not the same as whatever the BBOX parameter does
+		#once I figure out the equivalent swig call we can get rid of the request processing
+		#zoomRectangle() maybe?
+		self.mapfile.loadOWSParameters(self.build_request(min_x, min_y, max_x, max_y))
+		#self.mapfile.setExtent(min_x, min_y, max_x, max_y)
+
 		img = self.mapfile.draw()
 
 		img_id = build_node_id(zoom_level, tile_x, tile_y)
-		result =  (img_id,
-				StringIO.StringIO(img.getBytes()))
+		result =  (img_id, StringIO.StringIO(img.getBytes()))
+
 		return result
+
+	def build_request(self, min_x, min_y, max_x, max_y):
+		wms_req = mapscript.OWSRequest()
+		wms_req.setParameter('MODE', 'WMS')
+		wms_req.setParameter('VERSION', '1.1.1')
+		wms_req.setParameter('FORMAT', 'image/png')
+		wms_req.setParameter('WIDTH', str(self.img_w))
+		wms_req.setParameter('HEIGHT', str(self.img_h))
+		wms_req.setParameter('SRS', 'EPSG:3857')
+		wms_req.setParameter('REQUEST', 'GetMap')
+		wms_req.setParameter('BBOX', ','.join(str(x) for x in [min_x, min_y, max_x, max_y]))
+		wms_req.setParameter('LAYERS', ','.join(self.layers) )
+
+		return wms_req
 
