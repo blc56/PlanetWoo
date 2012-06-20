ecd2ac2eac20ff2b7353eb8aea4edadf607249aa
Benjamin Campbell <benjamin.l.campbell@gmail.com>
2012-06-20 09:17:17 -0500

    Add scripts for distributed rendering.

--- fabric/dist_render.py
+++ fabric/dist_render.py
@@ -6,6 +6,8 @@ sys.path.append('../')
 import tiletree
 import os.path
 import math
+import uuid
+import StringIO
 
 def split_bbox(min_num_boxes, start_zoom, stop_zoom, min_x, min_y, max_x, max_y):
 	nodes = [tiletree.QuadTreeGenNode(None, min_x, min_y, max_x, max_y, start_zoom)]
@@ -36,7 +38,7 @@ def create_machine_jobs(global_config):
 
 	jobs_per_thread = int(math.ceil(len(jobs) / float(total_num_threads)))
 
-	render_node_configs = []
+	render_node_configs = {}
 	for render_node in global_config['render_nodes']:
 		this_num_jobs = min(jobs_per_thread * render_node['num_threads'], len(jobs))
 		this_config = {
@@ -56,11 +58,11 @@ def create_machine_jobs(global_config):
 				'stop_zoom': global_config['stop_zoom'],
 			})
 		del jobs[0:this_num_jobs]
-		render_node_configs.append(this_config)
+		render_node_configs[render_node['address']] = this_config
 
 	#add in any left over bits at the top of the tree
 	if(fill_to_zoom_level >= 0):
-		render_node_configs[0]['jobs'].append({
+		render_node_configs.values()[0]['jobs'].append({
 			'extent': global_config['map_extent'],
 			'start_zoom': global_config['start_zoom'],
 			'stop_zoom': fill_to_zoom_level
@@ -71,17 +73,40 @@ def create_machine_jobs(global_config):
 	return render_node_configs
 
 @parallel
-def copy_data_files(global_config):
-	for data_file in global_config['data_files']:
-		put(data_file, global_config['data_file_dest'])
+def copy_data_files(global_config, render_node_config):
+	#copy over mapfile, shapefile, and render config files
+	run('mkdir -p %s' % global_config['data_file_dest'])
 	put(global_config['mapfile_path'], global_config['data_file_dest'])
+	put(global_config['shapefile_path'], global_config['data_file_dest'])
+
+	tmp_config_bytes = StringIO.StringIO()
+	tmp_config_bytes.write(json.dumps(render_node_config))
+	tmp_file_name = str(uuid.uuid4()) + '.json'
+
+	remote_file_path = os.path.join(global_config['data_file_dest'], tmp_file_name)
+	put(tmp_config_bytes, remote_file_path)
+	return remote_file_path
+
+	#copy over user user specified data files
+	#for data_file in global_config['data_files']:
+		#put(data_file, global_config['data_file_dest'])
+
+@parallel
+def run_render_node(global_config, render_node_configs):
+	render_node_config = render_node_configs[env.host]
+	remote_config_path = copy_data_files(global_config, render_node_config)
+
+	#run('screen -S tiletree %s -c %s' % (global_config['render_script'], remote_config_path))
+	run('%s -c %s' % (global_config['render_script'], remote_config_path))
+
 
 @task
 @serial
 def render(config_path):
 	global_config=json.loads(open(config_path, 'r').read())
 	render_node_configs = create_machine_jobs(global_config)
-	render_hosts = [n['address'] for n in render_node_configs]
+	render_hosts = [n['address'] for n in render_node_configs.values()]
 
-	#execute(copy_data_files, hosts=render_hosts, global_config=global_config)
+	execute(run_render_node, hosts=render_hosts, global_config=global_config,
+			render_node_configs=render_node_configs)
 
