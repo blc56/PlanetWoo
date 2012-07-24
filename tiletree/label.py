6fb7c4923f9fcb3c01e0bcf58fae0bbc677ffc87
Benjamin Campbell <benjamin.l.campbell@gmail.com>
2012-07-24 18:48:15 +0000

    label collision checks with off tile labels.

--- tiletree/label.py
+++ tiletree/label.py
@@ -14,39 +14,11 @@ def bbox_check(label_bbox, tile_bbox):
 		return True
 	return False
 
-def position_label(shape, node, img_w, img_h, label_spacing, label_width, label_height):
-	seed_point = shape.getCentroid()
-
-	#if(shape.getValue(1) != 'IN'):
-		#return None
-
-	x_scale = (node.max_x - node.min_x) / float(img_w)
-	y_scale = (node.max_y - node.min_y) / float(img_h)
-	x_repeat_interval = label_spacing * x_scale
-	y_repeat_interval = label_spacing * y_scale
-	label_geo_w = label_width * x_scale * .5
-	label_geo_h = label_height * y_scale * .5
-
-	x_mid = (node.max_x - node.min_x) / 2.0
-	y_mid = (node.max_y - node.min_y) / 2.0
-	x_spaces = math.floor((node.max_x - seed_point.x)/float(x_repeat_interval) + .5)
-	y_spaces = math.floor((node.max_y - seed_point.y)/float(y_repeat_interval) + .5)
-
-	ghost_x = seed_point.x + x_spaces * x_repeat_interval
-	ghost_y = seed_point.y + y_spaces * y_repeat_interval
-
-	label_geo_bbox = (ghost_x - label_geo_w, ghost_y - label_geo_h,
-			ghost_x + label_geo_w, ghost_y + label_geo_h) 
-	if(bbox_check(label_geo_bbox, (node.min_x, node.min_y, node.max_x, node.max_y))):
-		label_geo_bbox_shape = mapscript.shapeObj.fromWKT(tiletree.bbox_to_wkt(*label_geo_bbox))
-		if(shape.contains(label_geo_bbox_shape) ):
-			return label_geo_bbox
-	return None
-
 class LabelRenderer:
 	def __init__(self, mapfile_string, feature_storage_manager, label_col_index, map_extent,
 			min_zoom=0, max_zoom=19,
-			label_spacing=1024, img_w=256, img_h=256, tile_buffer=256, font='Utopia', font_size=12):
+			label_spacing=1024, img_w=256, img_h=256, tile_buffer=256, font='Utopia', font_size=12,
+			point_labels=True):
 		self.mapfile = mapscript.fromstring(mapfile_string)
 		self.feature_storage_manager = feature_storage_manager
 		self.label_col_index = label_col_index
@@ -59,11 +31,12 @@ class LabelRenderer:
 		self.font_size = font_size
 		self.min_zoom = min_zoom
 		self.max_zoom = max_zoom
+		self.point_labels = point_labels
 
 	def tile_info(self, node, check_full=True):
 		return (False, False, False)
 
-	def render_label(self, context, label_text, img_x, img_y, img_max_x, img_max_y):
+	def render_label(self, context, label_text, img_x, img_y, img_max_x, img_max_y, color):
 		context.move_to(img_x, img_y)
 		context.text_path(label_text)
 		context.fill()
@@ -75,7 +48,8 @@ class LabelRenderer:
 		context.line_to(img_max_x, img_max_y)
 		context.line_to(img_x, img_max_y)
 		context.line_to(img_x, img_y)
-		context.set_source_rgba(1, 0, 0, .5)
+		#context.set_source_rgba(1, 0, 0, .5)
+		context.set_source_rgba(*color)
 		context.stroke()
 
 	def get_label_size(self, surface, label_text):
@@ -92,6 +66,45 @@ class LabelRenderer:
 		surface.write_to_png(img_bytes)
 		return (img_id, img_bytes)
 
+	#returns (is_in_tile, bbox)
+	def position_label(self, shape, node, img_w, img_h, label_spacing, label_width, label_height):
+		seed_point = shape.getCentroid()
+
+		x_scale = (node.max_x - node.min_x) / float(img_w)
+		y_scale = (node.max_y - node.min_y) / float(img_h)
+		x_repeat_interval = label_spacing * x_scale
+		y_repeat_interval = label_spacing * y_scale
+		label_geo_w = label_width * x_scale * .5
+		label_geo_h = label_height * y_scale * .5
+
+		x_mid = (node.max_x - node.min_x) / 2.0
+		y_mid = (node.max_y - node.min_y) / 2.0
+		x_spaces = math.floor((node.max_x - seed_point.x)/float(x_repeat_interval) + .5)
+		y_spaces = math.floor((node.max_y - seed_point.y)/float(y_repeat_interval) + .5)
+
+		ghost_x = seed_point.x + x_spaces * x_repeat_interval
+		ghost_y = seed_point.y + y_spaces * y_repeat_interval
+
+		label_geo_bbox = (ghost_x - label_geo_w, ghost_y - label_geo_h,
+				ghost_x + label_geo_w, ghost_y + label_geo_h) 
+
+		is_in_tile = False
+		if(bbox_check(label_geo_bbox, (node.min_x, node.min_y, node.max_x, node.max_y))):
+			is_in_tile = True
+
+		label_geo_bbox_shape = mapscript.shapeObj.fromWKT(tiletree.bbox_to_wkt(*label_geo_bbox))
+		if(self.point_labels and (x_spaces < 1 and y_spaces < 1) ):
+			return (is_in_tile, label_geo_bbox)
+		elif(shape.contains(label_geo_bbox_shape) ):
+			return (is_in_tile, label_geo_bbox)
+		return None
+
+	def collision_check(self, node, check_bbox, label_bboxes):
+		for l in label_bboxes:
+			if(bbox_check(l, check_bbox)):
+				return True
+		return False
+
 	def render(self, node):
 		surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, self.img_w, self.img_h)
 		#convert from a pixel buffer distance to an image buffer distance
@@ -100,9 +113,10 @@ class LabelRenderer:
 		x_buffer = x_scale * self.tile_buffer
 		y_buffer = y_scale * self.tile_buffer
 
+		label_bboxes = []
+
 		rect = mapscript.rectObj(node.min_x - x_buffer, node.min_y - y_buffer,
 				node.max_x + x_buffer, node.max_y + y_buffer)
-
 		self.mapfile.queryByRect(rect)
 
 		for x in range(self.mapfile.numlayers):
@@ -114,18 +128,31 @@ class LabelRenderer:
 				shape = layer.getShape(result)
 				label_text = unicode(shape.getValue(self.label_col_index), 'latin_1')
 				context, label_width, label_height = self.get_label_size(surface, label_text)
-				label_extent = position_label(shape, node, self.img_w, self.img_h, self.label_spacing,
+
+				pos_results = self.position_label(shape, node, self.img_w, self.img_h, self.label_spacing,
 						label_width, label_height)
-				if(label_extent == None):
+
+				if(pos_results == None):
 					continue
-				#TODO: check collisions from other labels and reposition
+
+				is_in_tile, label_extent = pos_results
+
+				color = (1, 0, 0, 1)
+				if(self.collision_check(node, label_extent, label_bboxes)):
+					color = (0, 1, 0, 1)
+					continue
+
+				label_bboxes.append(label_extent)
+				if(not is_in_tile):
+					continue
+
 				img_x, img_y = tiletree.geo_coord_to_img(label_extent[0], label_extent[1],
 						self.img_w, self.img_h, node.min_x, node.min_y, node.max_x, node.max_y)
 				img_max_x, img_max_y = tiletree.geo_coord_to_img(label_extent[2], label_extent[3],
 						self.img_w, self.img_h, node.min_x, node.min_y, node.max_x, node.max_y)
 
 				#TODO: add fontconfig with zoom level range and other options
-				self.render_label(context, label_text, img_x, img_y, img_max_x, img_max_y)
+				self.render_label(context, label_text, img_x, img_y, img_max_x, img_max_y, color)
 
 			layer.close()
 
