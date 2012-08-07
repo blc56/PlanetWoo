302c28b38c98b71edf7655761e788d7b2a2070c4
Benjamin Campbell <benjamin.l.campbell@gmail.com>
2012-08-07 20:59:16 +0000

    Add font ttf configurability. Fix label positioning bug.

--- tiletree/label.py
+++ tiletree/label.py
@@ -12,6 +12,66 @@ import tiletree
 
 ALMOST_ZERO = float(1e-10)
 
+import ctypes
+import cairo
+
+#shameless copy and paste from http://cairographics.org/freetypepython/
+CAIRO_STATUS_SUCCESS = 0
+FT_Err_Ok = 0
+
+# find shared objects
+_freetype_so = ctypes.CDLL ("libfreetype.so.6")
+_cairo_so = ctypes.CDLL ("libcairo.so.2")
+
+_cairo_so.cairo_ft_font_face_create_for_ft_face.restype = ctypes.c_void_p
+_cairo_so.cairo_ft_font_face_create_for_ft_face.argtypes = [ ctypes.c_void_p, ctypes.c_int ]
+_cairo_so.cairo_set_font_face.argtypes = [ ctypes.c_void_p, ctypes.c_void_p ]
+_cairo_so.cairo_font_face_status.argtypes = [ ctypes.c_void_p ]
+_cairo_so.cairo_status.argtypes = [ ctypes.c_void_p ]
+
+# initialize freetype
+_ft_lib = ctypes.c_void_p ()
+if FT_Err_Ok != _freetype_so.FT_Init_FreeType (ctypes.byref (_ft_lib)):
+  raise "Error initialising FreeType library."
+
+class PycairoContext(ctypes.Structure):
+	_fields_ = [("PyObject_HEAD", ctypes.c_byte * object.__basicsize__),
+		("ctx", ctypes.c_void_p),
+		("base", ctypes.c_void_p)]
+
+_surface = cairo.ImageSurface (cairo.FORMAT_A8, 0, 0)
+
+def create_cairo_font_face_for_file (filename, faceindex=0, loadoptions=0):
+	global _freetype_so
+	global _cairo_so
+	global _ft_lib
+	global _surface
+
+	filename = str(filename)
+
+	# create freetype face
+	ft_face = ctypes.c_void_p()
+	cairo_ctx = cairo.Context (_surface)
+	cairo_t = PycairoContext.from_address(id(cairo_ctx)).ctx
+
+	if FT_Err_Ok != _freetype_so.FT_New_Face (_ft_lib, filename, faceindex, ctypes.byref(ft_face)):
+		raise Exception("Error creating FreeType font face for " + filename)
+
+	# create cairo font face for freetype face
+	cr_face = _cairo_so.cairo_ft_font_face_create_for_ft_face (ft_face, loadoptions)
+	if CAIRO_STATUS_SUCCESS != _cairo_so.cairo_font_face_status (cr_face):
+		raise Exception("Error creating cairo font face for " + filename)
+
+	_cairo_so.cairo_set_font_face (cairo_t, cr_face)
+	if CAIRO_STATUS_SUCCESS != _cairo_so.cairo_status (cairo_t):
+		raise Exception("Error creating cairo font face for " + filename)
+
+	face = cairo_ctx.get_font_face ()
+
+	return face
+
+#end shameless copy/paste
+
 def bbox_check(label_bbox, tile_bbox):
 	if(label_bbox[0] <= tile_bbox[2] and label_bbox[2] >= tile_bbox[0] and
 		label_bbox[1] <= tile_bbox[3] and label_bbox[3] >= tile_bbox[1]):
@@ -187,9 +247,10 @@ class LabelRenderer:
 	def get_label_size(self, surface, label_text, label_class):
 		context = cairo.Context(surface)
 		weight = cairo.FONT_WEIGHT_NORMAL
-		if(label_class.weight == "bold"):
-			weight = cairo.FONT_WEIGHT_BOLD
-		font_face = context.select_font_face(label_class.font, cairo.FONT_SLANT_NORMAL, )
+		#if(label_class.weight == "bold"):
+			#weight = cairo.FONT_WEIGHT_BOLD
+		#font_face = context.select_font_face(label_class.font, cairo.FONT_SLANT_NORMAL, )
+		font_face = create_cairo_font_face_for_file(label_class.font, 0)
 		context.set_font_face(font_face)
 		context.set_font_size(label_class.font_size)
 		width, height = self.get_line_size(context, label_text)
@@ -317,11 +378,6 @@ class LabelRenderer:
 			label_line = self.build_label_line(y_pos, min_label_x, max_label_x)
 			#make sure the label is contained by its corresponding geometry
 			label_line = label_line.intersection(shape)
-			if(not label_line):
-				continue
-			#make sure the label doesn't collided with any other labels
-			if(node.label_geoms != None):
-				label_line = label_line.difference(node.label_geoms)
 			if(not label_line):
 				continue
 
