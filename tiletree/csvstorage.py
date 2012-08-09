a024d09532ecf20b074ffe72193c35abd7491dc5
Benjamin Campbell <benjamin.l.campbell@gmail.com>
2012-08-09 21:09:38 +0000

    Copyright info and place under GPLv3.

--- tiletree/csvstorage.py
+++ tiletree/csvstorage.py
@@ -1,3 +1,20 @@
+#Copyright (C) 2012 Excensus, LLC.
+#
+#This file is part of PlanetWoo.
+#
+#PlanetWoo is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#
+#PlanetWoo is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with PlanetWoo.  If not, see <http://www.gnu.org/licenses/>.
+
 ##\file csvstorage.py storage classes for the tiletree module. 
 import tiletree
 import os
