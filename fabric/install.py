1283975493da9a65588f2419065387108be93707
Benjamin Campbell <benjamin.l.campbell@gmail.com>
2012-08-16 08:59:35 -0500

    Add google projection to proj on install scripts.

--- fabric/install.py
+++ fabric/install.py
@@ -224,6 +224,11 @@ def install_deps(prefix="/opt/planetwoo/"):
 			with cd('mapscript/python'):
 				sudo('export PYTHONPATH=%(prefix)s/lib/python2.7/site-packages/ ; python setup.py install --prefix %(prefix)s' % {'prefix': prefix})
 
+	#add the google projection
+	sudo('echo -e "# google projection\\n<900913> +proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs <>" >> /usr/share/proj/epsg')
+
+
+
 @task
 def install_planetwoo(prefix="/opt/planetwoo"):
 	sudo('mkdir -p %s' % prefix)
